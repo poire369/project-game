0f35e4665d7530b7016f42de1bc96392d21665ab
Nico <nicolasnoel369@gmail.com>
2018-05-21 17:49:53 +0200

    init(project)

--- src/app/Main.java
+++ src/app/Main.java
@@ -11,8 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("../view/Board.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 800, 500));
+        primaryStage.setScene(new Scene(root, 1295, 918));
         primaryStage.show();
     }
 
--- src/controller/BoardController.java
+++ src/controller/BoardController.java
@@ -2,7 +2,17 @@ package controller;
 
 import javafx.fxml.FXML;
 
+import java.awt.event.ActionEvent;
+
 public class BoardController {
+
+    public void action(ActionEvent event){
+        System.out.println(event.getID());
+        System.out.println(event.getActionCommand());
+        System.out.println(event.getModifiers());
+        System.out.println(event.paramString());
+        System.out.println(event.getSource().toString());
+    }
 
 }
 
